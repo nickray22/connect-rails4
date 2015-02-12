c68418d71dc1e86db13595e0e7bfdf1148ee255c
Brit Butler <redline6561@gmail.com>
2015-02-12 14:33:55 -0500

    Validations for proper number of players.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,6 @@
 class Game < ActiveRecord::Base
   has_many :players
   has_many :users, through: :players
+
+  validates_length_of :users, maximum: 2, message: "can have at most two players."
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,6 @@
 class Player < ActiveRecord::Base
   belongs_to :game
   belongs_to :user
+
+  validates_uniqueness_of :user_id, :scope => :game_id
 end
