24cee406bee1fa2e34983e65a96d47be9c25da85
Brit Butler <redline6561@gmail.com>
2015-02-13 12:28:19 -0500

    Add some useful scopes to the Game model.

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,7 @@ class Game < ActiveRecord::Base
   has_many :users, through: :players
 
   validates_length_of :users, maximum: 2, message: "can have at most two players."
+
+  scope :waiting, -> { where(:players_count => 1) }
+  scope :active, -> { where(:finished => false) }
 end
